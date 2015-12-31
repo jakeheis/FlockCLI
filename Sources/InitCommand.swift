76db0934ccb89455aec201db375e288faf842c46
Jake Heiser <jake@quizlet.com>
2015-12-31 15:34:00 -0600

    Production config file

--- Sources/InitCommand.swift
+++ Sources/InitCommand.swift
@@ -31,6 +31,17 @@ class InitCommand: CommandType {
       // Symlink Flockfile.swift to deploy/flock/main.swift
       let mainFlockURL = flockURL.URLByAppendingPathComponent("main.swift")
       try NSFileManager().createSymbolicLinkAtURL(mainFlockURL, withDestinationURL: flockfileURL)
+      
+      // Create deploy/production.swift
+      let productionURL = deployURL.URLByAppendingPathComponent("production.swift")
+      guard !productionURL.checkResourceIsReachableAndReturnError(nil) else {
+        throw CLIError.Error("deploy/production.swift must not already exist")
+      } 
+      try productionDefault().writeToURL(productionURL, atomically: true, encoding: NSUTF8StringEncoding)
+      
+      // Symlink deploy/production.swift to deploy/flock/production.swift
+      let productionFlockURL = flockURL.URLByAppendingPathComponent("production.swift")
+      try NSFileManager().createSymbolicLinkAtURL(productionFlockURL, withDestinationURL: productionURL)
     }
     
     // MARK: - Defaults
@@ -51,12 +62,27 @@ class InitCommand: CommandType {
   
     private func flockfileDefault() -> String {
       return [
-      "import Flock",
-      "",
-      "Flock.use(Flock.Default)",
-      "",
-      "Flock.run()",
-      ""
+        "import Flock",
+        "",
+        "Flock.use(Flock.Default)",
+        "",
+        "Flock.addConfiguration(Production(), .Environment(\"production\")) // Located at deploy/production.swift",
+        "",
+        "Flock.run()",
+        ""
+      ].joinWithSeparator("\n")
+    }
+    
+    private func productionDefault() -> String {
+      return [
+        "import Flock",
+        "",
+        "class Production: Configuration {",
+        "    func configure() {",
+        "        // Flock.Deploy.quickly = true",
+        "    }",
+        "}",
+        ""
       ].joinWithSeparator("\n")
     }
   
