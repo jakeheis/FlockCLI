6f8d575347f42d8a284b21b44059e9b09758555a
Jake Heiser <jakeheiser1@gmail.com>
2016-10-26 12:08:26 -0700

    More instructions

--- Sources/InitCommand.swift
+++ Sources/InitCommand.swift
@@ -59,9 +59,11 @@ class InitCommand: Command {
         print("Steps to take to ensure Flock works correctly:".yellow)
         print("1. Update the required fields in deploy/Always.swift")
         print("2. Add your production and staging servers to deploy/Production.swift and deploy/Staging.swift respectively")
-        print("3. To add Flock dependencies:")
-        print("   a. Add the url and version to deploy/FlockDependencies.swift")
-        print("   b. in your Flockfile, at the top of the file import your dependency and below add `Flock.use(Dependency)`")
+        print("3. In your project's Package.swift, add `deploy` to the ignore list")
+        print()
+        print("To add Flock dependencies:")
+        print("1. Add the url and version of the dependency to deploy/FlockDependencies.json")
+        print("2. in your Flockfile, at the top of the file import your dependency and below add `Flock.use(Dependency)`")
     }
     
     // MARK: - Defaults
