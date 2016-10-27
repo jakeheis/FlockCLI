e443a497d970595f583e9e08459e5f67cde6d902
Jake Heiser <jakeheiser1@gmail.com>
2016-10-26 19:36:54 -0700

    Fixed

--- Sources/InitCommand.swift
+++ Sources/InitCommand.swift
@@ -28,6 +28,8 @@ class InitCommand: FlockCommand {
         build()
         
         print("Successfully initialized Flock!".green)
+        
+        printInstructions()
     }
     
     func checkExisting() throws {
@@ -126,21 +128,21 @@ class InitCommand: FlockCommand {
             "    let dependencies = json[\"dependencies\"] {",
             "    ",
             "    for dependency in dependencies {",
-            "        guard let name = dependency[\"name\"] as? String else {",
+            "        guard let url = dependency[\"url\"] as? String else {",
             "            print(\"Ignoring invalid dependency \\(dependency)\")",
             "            continue",
             "        }",
             "        let dependencyPackage: Package.Dependency",
             "        if let version = dependency[\"version\"] as? String, let packageVersion = Version(version) {",
-            "            dependencyPackage = Package.Dependency.Package(url: name, packageVersion)",
+            "            dependencyPackage = Package.Dependency.Package(url: url, packageVersion)",
             "        } else if let major = dependency[\"major\"] as? Int {",
             "            if let minor = dependency[\"minor\"] as? Int {",
-            "                dependencyPackage = Package.Dependency.Package(url: name, majorVersion: major, minor: minor)",
+            "                dependencyPackage = Package.Dependency.Package(url: url, majorVersion: major, minor: minor)",
             "            } else {",
-            "                dependencyPackage = Package.Dependency.Package(url: name, majorVersion: major)",
+            "                dependencyPackage = Package.Dependency.Package(url: url, majorVersion: major)",
             "            }",
             "        } else {",
-            "            print(\"Ignoring invalid dependency \\(name)\")",
+            "            print(\"Ignoring invalid dependency \\(url)\")",
             "            continue",
             "        }",
             "        package.dependencies.append(dependencyPackage)",
@@ -170,7 +172,7 @@ class InitCommand: FlockCommand {
             "{",
             "   \"dependencies\" : [",
             "       {",
-            "           \"name\" : \"/Users/jakeheiser/Documents/Swift/Flock\",",
+            "           \"url\" : \"/Users/jakeheiser/Documents/Swift/Flock\",",
             "           \"version\": \"0.0.1\"",
             "       }",
             "   ]",
